ea865cdd7e8e3fa2d5ea3b18e2d49adcbb604c21
JuXinyan <12111142@mail.sustech.edu.cn>
2024-05-16 22:16:56 +0800

    FEAT:  implementing AI model to AI node

---
+++ gomoku_ai/src/ai_node.py
@@ -0,0 +1,60 @@
+#!usr/bin/env python3
+
+import rospy
+from gomoku_vision.msg import Position
+from __future__ import print_function
+import pickle
+from game import Board, Game
+from mcts_alphaZero import MCTSPlayer
+from policy_value_net_numpy import PolicyValueNetNumpy
+
+n = 5
+width, height = 8, 8
+"""
+In the open source code we called, board structure is quite different.
+TODO: modify board structure with subscribe position.
+"""
+
+class GomokuAINode:
+    def __init__(self):
+        self.board = Board(width=width, height=height, n_in_row=n)
+        self.game = Game(self.board)
+        self.move_pub = rospy.Publisher('next_move', Position, queue_size=10)
+        self.position_sub = rospy.Subscriber('piece_position', Position, self.piece_callback)
+        self.model_file = 'best_policy_8_8_5.model'
+        
+        # Load the policy-value network
+        policy_param = pickle.load(open(self.model_file, 'rb'), encoding='bytes')
+        self.best_policy = PolicyValueNetNumpy(width, height, policy_param)
+        self.mcts_player = MCTSPlayer(self.best_policy.policy_value_fn, c_puct=5, n_playout=400)
+    
+    def run(self):
+        rospy.spin()
+
+    def piece_callback(self, data):
+        # Update the board with the human move
+        human_move = self.board.location_to_move((data.x, data.y))
+        self.board.do_move(human_move)
+        
+        # Get the AI move
+        ai_move = self.mcts_player.get_action(self.board)
+        
+        # Convert AI move back to (x, y) position
+        ai_move_x, ai_move_y = self.board.move_to_location(ai_move)
+        
+        # Publish the AI move
+        ai_move_position = Position()
+        ai_move_position.x = ai_move_x
+        ai_move_position.y = ai_move_y
+        self.move_pub.publish(ai_move_position)
+        
+        # Update the board with the AI move
+        self.board.do_move(ai_move)
+
+if __name__ == '__main__':
+    rospy.init_node('gomoku_ai_node')
+    node = GomokuAINode()
+    try:
+        node.run()
+    except rospy.ROSInterruptException:
+        pass
